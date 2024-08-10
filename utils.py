18c02418a814b247faf49366153041c530118b2f
Marco Colognesi <marcocolognesi5@gmail.com>
2024-08-10 22:08:07 +0200

    Implemented function to split input text by a Langchain text splitter (main.py uses RecursiveCharacterTextSplitter) and create Langchain Documents objects from such split text and its metadata (source document, page number, split number)

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from pypdf import PdfReader
+from langchain_community.docstore.document import Document
 
 
 def cast_str_to_path(filepath: str | Path) -> Path:
@@ -37,3 +38,36 @@ def pdf_loader(pdf_filepath: str | Path) -> list[dict]:
             }
         )
     return content
+
+
+def create_documents_from_chunks(content: list[dict], text_splitter) -> list[Document]:
+    """Create Document objects from the extracted text content.
+
+    First, it iterates through each entry in the input list, splitting the text using the provided text splitter.
+    Then, it creates a Document object for each split, adding metadata about the source document and page number.
+
+    Args:
+        content (list[dict]): A list of dictionaries containing the text, name of source document and page number.
+        text_splitter: The Langchain text splitter object.
+
+    Returns:
+        list[Document]: A list of Document objects.
+    """
+    document_list = []
+    for entry in content:
+        # Split the text
+        text_split = text_splitter.split_text(entry['text'])
+        document = entry['document']
+        page = entry['page']
+        # Create a Document object for each split
+        for idx, split in enumerate(text_split):
+            doc = Document(
+                page_content=split,
+                metadata={
+                    'document': document,
+                    'page': page,
+                    'split': idx
+                }
+            )
+            document_list.append(doc)
+    return document_list
